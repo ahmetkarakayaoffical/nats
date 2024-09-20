40f0db2e9fbfeccc249fad4475a9d804b4cc7df0
Miguel Cabrerizo <doncicuto@gmail.com>
2024-09-20 13:23:11 +0200

    fix: add From to Notification struct

--- data.go
+++ data.go
@@ -1,6 +1,7 @@
 package openuem_nats
 
 type Notification struct {
+	From    string `json:"from,omitempty"`
 	To      string `json:"to,omitempty"`
 	Subject string `json:"subject,omitempty"`
 	Message string `json:"message,omitempty"`
