1483f76bbcfdc043f37ce2dae6511aa89b48e808
Miguel Cabrerizo <doncicuto@gmail.com>
2024-09-22 13:38:05 +0200

    fix: add message action url to Notification

--- data.go
+++ data.go
@@ -1,11 +1,12 @@
 package openuem_nats
 
 type Notification struct {
-	From            string `json:"from,omitempty"`
-	To              string `json:"to,omitempty"`
-	Subject         string `json:"subject,omitempty"`
-	MessageTitle    string `json:"message_title,omitempty"`
-	MessageGreeting string `json:"message_greeting,omitempty"`
-	MessageText     string `json:"message_text,omitempty"`
-	MessageAction   string `json:"message_action,omitempty"`
+	From             string `json:"from,omitempty"`
+	To               string `json:"to,omitempty"`
+	Subject          string `json:"subject,omitempty"`
+	MessageTitle     string `json:"message_title,omitempty"`
+	MessageGreeting  string `json:"message_greeting,omitempty"`
+	MessageText      string `json:"message_text,omitempty"`
+	MessageAction    string `json:"message_action,omitempty"`
+	MessageActionURL string `json:"message_action_url,omitempty"`
 }
