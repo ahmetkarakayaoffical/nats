607a1c37b587712c1e2933925d4c1dadb0efa127
Miguel Cabrerizo <doncicuto@gmail.com>
2024-09-22 18:11:46 +0200

    feat: create CertificationRequest

--- data.go
+++ data.go
@@ -12,3 +12,17 @@ type Notification struct {
 	MessageAttachFileName string `json:"message_attach_filename,omitempty"`
 	MessageAttachFile     string `json:"message_attach_file,omitempty"`
 }
+
+type CertificationRequest struct {
+	Username     string `json:"username,omitempty"`
+	Organization string `json:"organization,omitempty"`
+	Country      string `json:"country,omitempty"`
+	Province     string `json:"province,omitempty"`
+	Locality     string `json:"locality,omitempty"`
+	Address      string `json:"address,omitempty"`
+	PostalCode   string `json:"postal_code,omitempty"`
+	YearsValid   int    `json:"years_valid,omitempty"`
+	MonthsValid  int    `json:"months_valid,omitempty"`
+	DaysValid    int    `json:"days_valid,omitempty"`
+	OCSP         string `json:"ocsp,omitempty"`
+}
