d814d31e24940b9340b1154921ef88f133a7ad0b
Miguel Cabrerizo <doncicuto@gmail.com>
2024-09-22 17:05:59 +0200

    feat: add attach file to notification

--- data.go
+++ data.go
@@ -1,12 +1,13 @@
 package openuem_nats
 
 type Notification struct {
-	From             string `json:"from,omitempty"`
-	To               string `json:"to,omitempty"`
-	Subject          string `json:"subject,omitempty"`
-	MessageTitle     string `json:"message_title,omitempty"`
-	MessageGreeting  string `json:"message_greeting,omitempty"`
-	MessageText      string `json:"message_text,omitempty"`
-	MessageAction    string `json:"message_action,omitempty"`
-	MessageActionURL string `json:"message_action_url,omitempty"`
+	From              string `json:"from,omitempty"`
+	To                string `json:"to,omitempty"`
+	Subject           string `json:"subject,omitempty"`
+	MessageTitle      string `json:"message_title,omitempty"`
+	MessageGreeting   string `json:"message_greeting,omitempty"`
+	MessageText       string `json:"message_text,omitempty"`
+	MessageAction     string `json:"message_action,omitempty"`
+	MessageActionURL  string `json:"message_action_url,omitempty"`
+	MessageAttachFile string `json:"message_attach_file,omitempty"`
 }
