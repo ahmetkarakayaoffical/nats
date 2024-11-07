70528c5f20394ee1eaed8cb1660ec8c1cb3a346a
Miguel Cabrerizo <doncicuto@gmail.com>
2024-11-07 12:59:28 +0100

    feat: register status slice

--- register_status.go
+++ register_status.go
@@ -1,5 +1,9 @@
 package openuem_nats
 
-const CERTIFICATE_SENT = "users.certificate_sent"
+const REGISTER_CERTIFICATE_SENT = "users.certificate_sent"
 const REGISTER_COMPLETE = "users.completed"
 const REGISTER_IN_REVIEW = "users.review_request"
+
+func RegisterStatus() []string {
+	return []string{REGISTER_CERTIFICATE_SENT, REGISTER_COMPLETE, REGISTER_IN_REVIEW}
+}
