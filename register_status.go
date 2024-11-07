c81130ee7d70d4db3429a83c4cc5789d4d3f39ba
Miguel Cabrerizo <doncicuto@gmail.com>
2024-11-07 13:00:04 +0100

    fix: rename register status

--- register_status.go
+++ register_status.go
@@ -4,6 +4,6 @@ const REGISTER_CERTIFICATE_SENT = "users.certificate_sent"
 const REGISTER_COMPLETE = "users.completed"
 const REGISTER_IN_REVIEW = "users.review_request"
 
-func RegisterStatus() []string {
+func RegisterPossibleStatus() []string {
 	return []string{REGISTER_CERTIFICATE_SENT, REGISTER_COMPLETE, REGISTER_IN_REVIEW}
 }
