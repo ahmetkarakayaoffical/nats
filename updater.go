1e62c1905e5441c5c9d9dac736908d200966aec7
Miguel Cabrerizo <doncicuto@gmail.com>
2024-11-11 14:56:03 +0100

    fix: download hash is now a string

--- updater.go
+++ updater.go
@@ -4,7 +4,7 @@ import "time"
 
 type OpenUEMUpdateRequest struct {
 	DownloadFrom string    `json:"download,omitempty"`
-	DownloadHash []byte    `json:"download_hash,omitempty"`
+	DownloadHash string    `json:"download_hash,omitempty"`
 	UpdateAt     time.Time `json:"updated_at,omitempty"`
 	UpdateNow    bool      `json:"update_now,omitempty"`
 }
