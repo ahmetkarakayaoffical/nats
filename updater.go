3fb881d360fb6ec3262157509ed05dee4a68196f
Miguel Cabrerizo <doncicuto@gmail.com>
2024-11-11 10:02:13 +0100

    fix: add download hash

--- updater.go
+++ updater.go
@@ -4,6 +4,7 @@ import "time"
 
 type OpenUEMUpdateRequest struct {
 	DownloadFrom string    `json:"download,omitempty"`
+	DownloadHash []byte    `json:"download_hash,omitempty"`
 	UpdateAt     time.Time `json:"updated_at,omitempty"`
 	UpdateNow    bool      `json:"update_now,omitempty"`
 }
