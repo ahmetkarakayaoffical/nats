b3044831439594379d87c2e2e1078131ebc5cc3d
Miguel Cabrerizo <doncicuto@gmail.com>
2024-11-13 16:37:41 +0100

    feat: update agent task exchange structs

--- updater.go
+++ updater.go
@@ -13,3 +13,11 @@ type OpenUEMRollbackRequest struct {
 	RollbackAt  time.Time `json:"updated_at,omitempty"`
 	RollbackNow bool      `json:"update_now,omitempty"`
 }
+
+const UPDATE_ERROR = "admin.update.agents.task_status_error"
+const UPDATE_PENDING = "admin.update.agents.task_status_pending"
+const UPDATE_SUCCESS = "admin.update.agents.task_status_success"
+
+func TaskUpdatePossibleStatus() []string {
+	return []string{UPDATE_ERROR, UPDATE_PENDING, UPDATE_SUCCESS}
+}
