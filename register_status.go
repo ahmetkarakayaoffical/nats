4fce96bdc1aafb8ded0dc4bcf8aff2aa7f282f5a
Miguel Cabrerizo <doncicuto@gmail.com>
2024-11-19 20:31:14 +0100

    feat: add register status to show that a certificate has been revoked

--- register_status.go
+++ register_status.go
@@ -3,6 +3,7 @@ package openuem_nats
 const REGISTER_CERTIFICATE_SENT = "users.certificate_sent"
 const REGISTER_COMPLETE = "users.completed"
 const REGISTER_IN_REVIEW = "users.review_request"
+const REGISTER_REVOKED = "users.certificate_revoked"
 
 func RegisterPossibleStatus() []string {
 	return []string{REGISTER_CERTIFICATE_SENT, REGISTER_COMPLETE, REGISTER_IN_REVIEW}
