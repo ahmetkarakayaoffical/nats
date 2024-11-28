2603709514840df994af0586583995930bc4f04b
Miguel Cabrerizo <doncicuto@gmail.com>
2024-11-28 12:43:13 +0100

    feat: add OpenUEMRelease for data exchange

--- updater.go
+++ updater.go
@@ -3,11 +3,27 @@ package openuem_nats
 import "time"
 
 type OpenUEMUpdateRequest struct {
-	LatestVersion string    `json:"latest_version,omitempty"`
-	DownloadFrom  string    `json:"download,omitempty"`
-	DownloadHash  string    `json:"download_hash,omitempty"`
-	UpdateAt      time.Time `json:"updated_at,omitempty"`
-	UpdateNow     bool      `json:"update_now,omitempty"`
+	DownloadFrom string    `json:"download,omitempty"`
+	DownloadHash string    `json:"download_hash,omitempty"`
+	UpdateAt     time.Time `json:"updated_at,omitempty"`
+	UpdateNow    bool      `json:"update_now,omitempty"`
+}
+
+type FileInfo struct {
+	Arch     string `json:"arch,omitempty"`
+	Os       string `json:"os,omitempty"`
+	FileURL  string `json:"file_url,omitempty"`
+	Checksum string `json:"checksum,omitempty"`
+}
+
+type OpenUEMRelease struct {
+	Version         string     `json:"version,omitempty"`
+	Channel         string     `json:"channel,omitempty"`
+	Summary         string     `json:"summary,omitempty"`
+	ReleaseNotesURL string     `json:"release_notes,omitempty"`
+	ReleaseDate     time.Time  `json:"release_date,omitempty"`
+	Files           []FileInfo `json:"files,omitempty"`
+	IsCritical      bool       `json:"is_critical,omitempty"`
 }
 
 const UPDATE_ERROR = "admin.update.agents.task_status_error"
