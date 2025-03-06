10358d865dddc48bca1928cbdae15ea6ac41b1b2
Miguel Cabrerizo <doncicuto@gmail.com>
2025-03-06 10:09:14 +0100

    feat: add wincfg nats structs

--- winget.go
+++ winget.go
@@ -4,3 +4,17 @@ type WingetPackage struct {
 	ID   string `json:"ID,omitempty"`
 	Name string `json:"name,omitempty"`
 }
+
+type WingetCfgProfiles struct {
+	AgentID string `json:"agentID,omitempty"`
+}
+
+type WingetCfgReport struct {
+	ProfileID string `json:"profileID,omitempty"`
+	Success   bool   `json:"success,omitempty"`
+}
+
+type WingetCfgDeploy struct {
+	PackageID string `json:"PackageID,omitempty"`
+	Installed bool   `json:"installed,omitempty"`
+}
