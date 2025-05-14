3bae96c530801bea7441da7511cce719fdd69507
Miguel Cabrerizo <doncicuto@gmail.com>
2025-05-14 14:55:48 +0200

    feat: add unknown system update status

--- system_update_settings.go
+++ system_update_settings.go
@@ -1,5 +1,6 @@
 package nats
 
+const UNKNOWN = "systemupdate.unknown"
 const NOT_CONFIGURED = "systemupdate.not_configured"
 const DISABLED = "systemupdate.disabled"
 const NOTIFY_BEFORE_DOWNLOAD = "systemupdate.notify_before_download"
@@ -7,5 +8,5 @@ const NOTIFY_BEFORE_INSTALLATION = "systemupdate.notify_before_installation"
 const NOTIFY_SCHEDULED_INSTALLATION = "systemupdate.notify_scheduled_installation"
 
 func SystemUpdatePossibleStatus() []string {
-	return []string{NOT_CONFIGURED, DISABLED, NOTIFY_BEFORE_DOWNLOAD, NOTIFY_BEFORE_INSTALLATION, NOTIFY_SCHEDULED_INSTALLATION}
+	return []string{UNKNOWN, NOT_CONFIGURED, DISABLED, NOTIFY_BEFORE_DOWNLOAD, NOTIFY_BEFORE_INSTALLATION, NOTIFY_SCHEDULED_INSTALLATION}
 }
