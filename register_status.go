45922fb3baa4d2550c39018902bb8d6b45c88d49
Miguel Cabrerizo <doncicuto@gmail.com>
2025-07-17 12:17:00 +0200

    feat: add register approved const

--- register_status.go
+++ register_status.go
@@ -4,6 +4,7 @@ const REGISTER_CERTIFICATE_SENT = "users.certificate_sent"
 const REGISTER_COMPLETE = "users.completed"
 const REGISTER_IN_REVIEW = "users.review_request"
 const REGISTER_REVOKED = "users.certificate_revoked"
+const REGISTER_APPROVED = "users.approved"
 
 func RegisterPossibleStatus() []string {
 	return []string{REGISTER_CERTIFICATE_SENT, REGISTER_COMPLETE, REGISTER_IN_REVIEW}
